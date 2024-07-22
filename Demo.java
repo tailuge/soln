55c617d876d61532d61d7eeabcedd44888527011
tailuge <tailuge@gmail.com>
2024-07-22 16:48:01 +0000

    min

---
+++ Demo.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Demo {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("hello", "there");
+		System.out.println(names);
+	}
+}
